c6d0376bf3be5cb8ba92bfd34bcfbf45eb4b5d81
philcable <phil@happyprime.co>
2021-03-05 15:32:55 -0800

    Enqueue JS with block style filtering
    This includes examples of both adding and removing styles from the
    button block.

---
+++ themes/atvp/js/src/block-styles.js
@@ -0,0 +1,15 @@
+// WordPress dependencies
+import { __ } from '@wordpress/i18n';
+
+wp.domReady( () => {
+	// Register styles for the button block.
+	wp.blocks.registerBlockStyle( 'core/button',
+		{
+			name: 'arrow-link',
+			label: __( 'Arrow link' ),
+		}
+	);
+
+	// Unregister the "Outline" style from the button block.
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+} );
