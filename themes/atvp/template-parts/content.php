12b37c281dac83b821e1bb26c129ab07c084bf47
slocker <slocker@sjlocks.com>
2021-03-25 12:28:12 -0700

    Add excerpt and style archive

--- themes/atvp/template-parts/content.php
+++ themes/atvp/template-parts/content.php
@@ -25,27 +25,35 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'atvp' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		if ( is_singular() ) :
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'atvp' ),
-				'after'  => '</div>',
-			)
-		);
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'swingthefly' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'swingthefly' ),
+					'after'  => '</div>',
+				)
+			);
+
+		else :
+
+			the_excerpt();
+
+		endif;
 		?>
 	</div><!-- .entry-content -->
 
